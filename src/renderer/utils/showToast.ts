7d3295c72b8ee022dba1562f05fe8e4ff750888f
tangbzai <Tan-jc@qq.com>
2022-08-11 00:18:37 +0800

    chore: init

---
+++ src/renderer/utils/showToast.ts
@@ -0,0 +1,18 @@
+export default function showToast(options?: {
+  title?: string;
+  duration?: number;
+}) {
+  const body = document.getElementsByTagName('body')[0];
+  const duration = options?.duration || 1500;
+  const toast = document.createElement('div');
+  toast.setAttribute('class', '__Toast__');
+  toast.innerText = options?.title || '';
+  toast.setAttribute(
+    'style',
+    `animation-duration: ${(duration * 12) / 10000}s`
+  );
+  body.appendChild(toast);
+  setTimeout(() => {
+    body.removeChild(toast);
+  }, (duration * 12) / 10);
+}
